98d008e1ff427de523c3ba1b12a7b588ff2649d6
sun449036002 <449036002@qq.com>
2018-07-04 11:27:35 +0800

    wxxcx

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 //app.js
+const API_URL = "https://talk.sun.zj.cn/v1/";
 App({
-  onLaunch: function () {
+  onLaunch: function() {
+    var self = this;
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // var logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    wx.request({
+      url: API_URL + 'user/check_login?session_key=' + (wx.getStorageSync("session_key") || ""),
+      success: function(res) {
+        if (!res.data.nickname) {
+          // 登录
+          wx.login({
+            success: res => {
+              // 发送 res.code 到后台换取 openId, sessionKey, unionId
+              wx.request({
+                url: API_URL + "user/login",
+                data: {
+                  code: res.code || "",
+                },
+                success: function(res) {
+                  console.log(res);
+                  if (res.data.session_key) {
+                    wx.setStorageSync('session_key', res.data.session_key);
+                    self.globalData.userInfo = {
+                      nickname: res.data.nickname,
+                      session_key: res.data.session_key
+                    };
+                  }
+                }
+              })
+            }
+          })
+        } else {
+          self.globalData.userInfo = {
+            nickname: res.data.nickname,
+            session_key: wx.getStorageSync("session_key")
+          };
+        }
       }
     })
+
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log("getSetting", res.authSetting['scope.userInfo']);
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
@@ -34,7 +66,7 @@ App({
     })
   },
   globalData: {
-    apiurl: "https://talk.sun.zj.cn/v1/",
+    apiurl: API_URL,
     rtmpurl: "https://www.sun.zj.cn/live",
     socketurl: "wss://talk.sun.zj.cn",
     userInfo: null
--- components/chat/chat.js
+++ components/chat/chat.js
@@ -5,9 +5,9 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    roomId : {
-      type:String,
-      value:""
+    roomId: {
+      type: String,
+      value: ""
     }
   },
 
@@ -15,12 +15,13 @@ Component({
    * 组件的初始数据
    */
   data: {
-    chatList:[],
-    msg : "",
-    isSocketOpen : false,
+    chatList: [],
+    msg: "",
+    isSocketOpen: false,
+    scrollTop: 0
   },
 
-  ready : function(res) {
+  ready: function(res) {
     this.init();
   },
 
@@ -29,59 +30,82 @@ Component({
    * 
    */
   methods: {
-    init:function(res) {
+    init: function(res) {
+      console.log(app.globalData);
       var self = this;
-      console.log(self.data);
-      return;
-      wx.connectSocket({
-        url: 'wss://talk.sun.zj.cn/wss',
-        data : {
-          roomId: self.data.roomId || "",
-          sessionKey: self.globalData.userinfo.sessionKey || ""
-        }
-      });
-      wx.onSocketOpen(function (res) {
+      setTimeout(function() {
+        wx.connectSocket({
+          url: 'wss://talk.sun.zj.cn/wss?roomId=' + (self.data.roomId || "") + "&sessionKey=" + app.globalData.userInfo.session_key,
+          data: {
+            roomId: self.data.roomId || "",
+            sessionKey: "" //app.globalData.userInfo.sessionKey || ""
+          }
+        });
+      }, 500);
+
+      wx.onSocketOpen(function(res) {
         console.log('WebSocket连接已打开！');
         self.setData({
-          isSocketOpen : true
+          isSocketOpen: true
         })
       });
-      wx.onSocketError(function (res) {
+      wx.onSocketError(function(res) {
         console.log('WebSocket连接打开失败，请检查！')
       });
-      wx.onSocketMessage(function (res) {
-        console.log('收到服务器内容：' + res.data)
+      wx.onSocketMessage(function(res) {
+        console.log('收到服务器内容：' + res.data);
+        var data = JSON.parse(res.data);
+        console.log("转换成JSON 对象：", data);
+        self.setData({
+          chatList: self.data.chatList.concat(data),
+          scrollTop: (self.data.chatList.length) * 1000
+        })
+
       });
-      wx.onSocketClose(function (res) {
+      wx.onSocketClose(function(res) {
         console.log('WebSocket 已关闭！')
       })
-      
+
     },
     /**
      * 输入聊天内容
      */
-    chatInputChange : function(e) {
+    chatInputChange: function(e) {
       this.setData({
-        msg : e.detail.value
+        msg: e.detail.value
       })
     },
     /**
      * 发送消息
      */
-    sendMsg : function(e) {
+    sendMsg: function(e) {
       var self = this;
-      if(!this.data.isSocketOpen) {
-        wx.showTabBar("Socket未打开");
+      if (!this.data.isSocketOpen) {
+        wx.showToast({
+          title: "Socket 未找开"
+        });
+        return;
+      }
+      if (self.data.msg.length == 0) {
+        wx.showToast({
+          title: "内容不得为空"
+        });
         return;
       }
       wx.sendSocketMessage({
-        data: self.data.msg,
-        complete : function(res) {
+        data: JSON.stringify({
+          Val: self.data.msg,
+          Sk: app.globalData.userInfo.session_key
+        }),
+        complete: function(res) {
           console.log(res);
+        },
+        fail: function(res) {
+          console.log("send socket message fail:", res)
         }
       });
       this.setData({
-        msg : ""
+        msg: ""
       })
     }
   }
